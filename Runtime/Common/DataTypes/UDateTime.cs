4c749c569fecea223b9dae240ccbaae77e66c18f
DukeGrimm <madsen.drew@gmail.com>
2024-07-22 14:26:09 -0500

    Updated UDateTime as Odin friendly way of managing time saving.

--- Runtime/Common/DataTypes/UDateTime.cs
+++ Runtime/Common/DataTypes/UDateTime.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 namespace EmberToolkit.Common.DataTypes
 {
-    [Serializable] // Mark the class as serializable for Unity
     public class UDateTime
     {
         public int Year;
@@ -23,6 +22,75 @@ namespace EmberToolkit.Common.DataTypes
             Minute = minute;
         }
 
+        public UDateTime(DateTime dateTime)
+        {
+            Year = dateTime.Year;
+            Month = dateTime.Month;
+            Day = dateTime.Day;
+            Hour = dateTime.Hour;
+            Minute = dateTime.Minute;
+        }
+
+        #region Operator Overloads
+
+        // Overload the == operator
+        public static bool operator ==(UDateTime uDateTime, DateTime dateTime)
+        {
+            return uDateTime.DateTime == dateTime;
+        }
+
+        // Overload the != operator
+        public static bool operator !=(UDateTime uDateTime, DateTime dateTime)
+        {
+            return !(uDateTime == dateTime);
+        }
+
+        // Overload the < operator
+        public static bool operator <(UDateTime uDateTime, DateTime dateTime)
+        {
+            return uDateTime.DateTime < dateTime;
+        }
+
+        // Overload the > operator
+        public static bool operator >(UDateTime uDateTime, DateTime dateTime)
+        {
+            return uDateTime.DateTime > dateTime;
+        }
+
+        // Overload the <= operator
+        public static bool operator <=(UDateTime uDateTime, DateTime dateTime)
+        {
+            return uDateTime.DateTime <= dateTime;
+        }
+
+        // Overload the >= operator
+        public static bool operator >=(UDateTime uDateTime, DateTime dateTime)
+        {
+            return uDateTime.DateTime >= dateTime;
+        }
+
+        // Remember to also override Equals and GetHashCode when overloading operators
+        public override bool Equals(object obj)
+        {
+            if (obj is UDateTime uDateTime)
+            {
+                return this.DateTime == uDateTime.DateTime;
+            }
+            if (obj is DateTime dateTime)
+            {
+                return this.DateTime == dateTime;
+            }
+            return false;
+        }
+
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return this.DateTime.GetHashCode();
+        }
+
+
         // Property to translate the UDateTime values into a DateTime
         public DateTime DateTime
         {
@@ -32,5 +100,14 @@ namespace EmberToolkit.Common.DataTypes
                 return new DateTime(Year, Month, Day, Hour, Minute, 0);
             }
         }
+
+        public void SetTime(DateTime inputTime)
+        {
+            Year = inputTime.Year;
+            Month = inputTime.Month;
+            Day = inputTime.Day;
+            Hour = inputTime.Hour;
+            Minute = inputTime.Minute;
+        }}
     }
 }
