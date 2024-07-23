f2e0143f3ecfb187329dba000cae7ba2244f0dbd
DukeGrimm <madsen.drew@gmail.com>
2024-07-22 23:57:45 -0500

    Adding Storage Container to OdinSerialize

--- Runtime/DataManagement/Data/StorageContainer.cs
+++ Runtime/DataManagement/Data/StorageContainer.cs
@@ -1,4 +1,5 @@
 ﻿using EmberToolkit.Common.Interfaces.Data;
+using Sirenix.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace EmberToolkit.DataManagement.Data
 {
     public class StorageContainer : IStorageContainer
     {
+        [OdinSerialize]
         public Dictionary<Type, object> RepositoryContainer { get; set; } = new Dictionary<Type, object>();
     }
 }
