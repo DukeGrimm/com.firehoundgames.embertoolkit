29eb5a415697df2b79c888495de8332197a8cd07
DukeGrimm <madsen.drew@gmail.com>
2026-05-17 06:25:28 -0500

    Add SceneConductor Base abstract class
    Imported the SceneConductor abstract class from the HexBit project.

---
+++ Runtime/Unity/Scenes/SceneConductorBase.cs
@@ -0,0 +1,41 @@
+﻿using EmberToolkit.Common.Interfaces.StateNodes;
+using EmberToolkit.Common.Interfaces.Unity.Behaviours.Managers.StateNodes;
+using EmberToolkit.Unity.Behaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace EmberToolkit.Unity.Scenes
+{
+    public abstract class SceneConductorBase<E> : EmberSingleton
+    {
+        protected IStateNodeManager<E> _stateManager;
+        protected IStateNode<E> _sceneStateNode;
+
+        [SerializeField] protected Selectable UI_SelectedOnSceneLoad;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            RequestService(out _stateManager);
+            //InitEventSubscriptions();
+            if (UI_SelectedOnSceneLoad != null) SelectOnLoadUIElement();
+        }
+
+        protected virtual void SelectOnLoadUIElement()
+        {
+            if (UI_SelectedOnSceneLoad != null) UI_SelectedOnSceneLoad.Select();
+        }
+
+        protected void LoadScene(E gameScene)
+        {
+            SceneManager.LoadScene(gameScene.ToString(), LoadSceneMode.Single);
+        }
+    }
+}
